a34b8196946651d755cf0b6b4a64d1cc1dab2214
PAN-Ziyue <ziyuepan99@outlook.com>
2022-04-07 14:32:24 +0800

    update os cli arguments

--- main.go
+++ main.go
@@ -5,11 +5,25 @@ import (
 	"GitHunter/model"
 	"GitHunter/repo"
 	"GitHunter/web"
+    "os"
+    "fmt"
 )
 
 func main() {
 	config.Init()
 	model.Init()
+    
+    if len(os.Args) < 2 {
+        fmt.Println("require an argument")
+    }
+        
+    switch os.Args[1] {
+    case "clone":
+        repo.Clone() 
+    case "crawl":
+        web.Crawl() 
+    }
+    
 	web.Crawl()
 	repo.Clone()
 }
